f717ceddb4ec207707fbcf676bce5533189d9da9
刘晓辉 <liuxiaohui@mogoroom.com>
2018-09-06 14:13:07 +0800

    commet

--- src/main/java/com/lxh/thread/philosopher/Chopsticks.java
+++ src/main/java/com/lxh/thread/philosopher/Chopsticks.java
@@ -7,11 +7,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 两种死锁避免算法：
+ *
+ * *进程启动拒绝：如果一个进程的请求会导致死锁，则不启动该进程。
+ *
+ * *资源分配拒绝：如果一个进程增加的资源请求会导致死锁，则不允许此分配(银行家算法)。
  * @author lxh
  * @Date 2018/6/22
  */
 public class Chopsticks {
     public static List<Boolean> chops = new ArrayList<Boolean>();
+    /** 工作向量work：表示系统可提供给进程继续运行的所需的各类资源的数目，执行安全算法开始时 work=available。**/
     static {
         //为了方便计算，第一个不会参与计算
         chops.add(false);
